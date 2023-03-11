0e156bed0c1865aababff062a689bc899b6ca085
Herman Kwok <hk_7090@outlook.com>
2023-03-11 21:07:10 +0800

    Create KanColle.bat

---
+++ KanColle/KanColle.bat
@@ -0,0 +1,4 @@
+ffmpeg -i 45-大井.flac -c:a flac -compression_level 12 D:\FFOutput\45-大井.flac
+ffmpeg -i 45-大井.mkv -c:v libx264rgb -qp 0 -preset placebo -c:a flac -compression_level 12 D:\FFOutput\45-大井.mkv
+ffmpeg -i 46-Iowa.mkv -c:v libx264rgb -qp 0 -preset placebo -c:a flac -compression_level 12 D:\FFOutput\46-Iowa.mkv
+ffmpeg -i 47-Warspite.mkv -c:v libx264rgb -qp 0 -preset placebo -c:a flac -compression_level 12 D:\FFOutput\47-Warspite.mkv
