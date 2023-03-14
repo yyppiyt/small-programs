e9dfa44115b9c7ce21435cc62ebeb970010c0402
Herman Kwok <hk_7090@outlook.com>
2023-03-14 23:53:10 +0800

    Complete BulkRename
    finished rename_file()

--- BulkRename/bulk-rename.py
+++ BulkRename/bulk-rename.py
@@ -1,6 +1,7 @@
 import sys, os, codecs, csv
 
 input_path, file_list, new_filename_list, rename_list = None, [], [], []
+success, fail = int(0), int(0)
 
 def print_debug_infos():
     print("\nDebug infos:")
@@ -44,11 +45,11 @@ def update_file_list():
         if not os.path.isfile(os.path.join(input_path, i)):
             file_list.remove(i)
     if not file_list:
-        sys.exit(f"0 file found at {input_path}")
-    load_rename_csv()
+        sys.exit(f"0 file found at {input_path}, program ended")
 
 def load_rename_csv():
     global new_filename_list
+    new_filename_list.clear()
     with codecs.open(f"{os.path.dirname(os.path.abspath(__file__))}/rename.csv", "r", "utf-8") as rename_csv:
         csv_reader = csv.reader(rename_csv)
         for row in csv_reader:
@@ -56,6 +57,7 @@ def load_rename_csv():
 
 def update_rename_list():
     global file_list, new_filename_list, rename_list
+    rename_list.clear()
     for i in range(len(new_filename_list)):
         if i >= len(file_list):
             break
@@ -67,15 +69,26 @@ def show_rename_list():
         print(f"\"{file_to_rename[0]}\" -> \"{file_to_rename[1]}\"")
 
 def rename_file():
-    # try:
-    #     print(os.rename(os.path.join(input_path, "test"), os.path.join(input_path, "test.txt")))
-    # except FileNotFoundError:
-    #     print(f"File not found: {}")
-    pass
+    global success, fail
+    for file_to_rename in rename_list:
+        try:
+            os.rename(os.path.join(input_path, file_to_rename[0]), os.path.join(input_path, file_to_rename[1]))
+            success += 1
+            print(f"\"{file_to_rename[0]}\" renamed to \"{file_to_rename[1]}\"")
+        except FileNotFoundError:
+            fail += 1
+            print(f"File not found: \"{file_to_rename[0]}\"")
+        except FileExistsError:
+            fail += 1
+            print(f"Can't rename \"{file_to_rename[0]}\" to \"{file_to_rename[1]}\", filename already exists")
+        except PermissionError:
+            fail += 1
+            print(f"Can't rename \"{file_to_rename[0]}\", file is being used")
 
 def setup():
     change_input_path()
     update_file_list()
+    load_rename_csv()
     update_rename_list()
     show_rename_list()
 
@@ -83,13 +96,20 @@ if __name__ == "__main__":
     setup()
     while True:
         ans = input("Confirm rename above files? (Y/N): ").lower()
-        if ans == "y":
-            print("yes")
-            break
-        elif ans == "n":
-            print("no")
-            break
+        if ans == "list":
+            show_rename_list()
+        if ans == "update":
+            print("Updating rename list...")
+            update_file_list()
+            load_rename_csv()
+            update_rename_list()
+            show_rename_list()
+        elif ans == "y" or ans == "yes":
+            rename_file()
+            sys.exit(f"Rename completed with {success} success and {fail} fail")
+        elif ans == "n" or ans == "no":
+            sys.exit("Rename aborted, program ended")
         else:
-            press_enter_to_continue("wat")
+            press_enter_to_continue("Unknown operation")
 
     # print_debug_infos()
